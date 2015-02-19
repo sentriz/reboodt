283c920d8caa9e22e52638ccc049dff97aed8a53
Senan Kelly <senan@senan.xyz>
2015-02-19 20:34:43 +0000

    init commit

---
+++ config.py
@@ -0,0 +1,30 @@
+# add as many as you can handle
+servers = [
+    {
+        'connect':  False,
+        'name':     "EsperNet",
+        'host':     "irc.esper.net",
+        'port':     6667,
+        'nick':     "rebooped",
+        'chans':    ["#tests"],
+        'auth':     (False, "password")
+    },
+    {
+        'connect':  False,
+        'name':     "EsperNet",
+        'host':     "irc.esper.net",
+        'port':     6667,
+        'nick':     "rebooped",
+        'chans':    ["#tests"],
+        'auth':     (False, "password")
+    },
+    {
+        'connect':  False,
+        'name':     "ExampleNet",
+        'host':     "irc.example.net",
+        'port':     6667,
+        'nick':     "bot_nick",
+        'chans':    ["#channel1", "channel2"],
+        'auth':     (False, "password")
+    }
+]
