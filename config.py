9673c3fc6bc1524c672f0330353ccb1d5268abbf
sentriz <senan.f.b.kelly+github@gmail.com>
2015-02-21 16:28:43 +0000

    added plugin support

--- config.py
+++ config.py
@@ -1,21 +1,24 @@
+admins = ["sentriz", "sentriz_android"]
+command_prefix = "."
+
 # add as many as you can handle
 servers = [
     {
-        'connect':  False,
-        'name':     "ExampleNet",
-        'host':     "irc.example.net",
+        'connect':  True,
+        'name':     "GeekShed",
+        'host':     "irc.geekshed.net",
         'port':     6667,
-        'nick':     "bot_nick",
-        'chans':    ["#channel1", "channel2"],
-        'auth':     (False, "password")
+        'nick':     "reboodt",
+        'chans':    ["#drpeen", "#testong"],
+        'auth':     (False, "password") #authenticate with NickServ? if True, type password too.
     },
     {
         'connect':  False,
-        'name':     "ExampleNet",
-        'host':     "irc.example.net",
+        'name':     "EsperNet",
+        'host':     "irc.esper.net",
         'port':     6667,
-        'nick':     "bot_nick",
-        'chans':    ["#channel1", "channel2"],
+        'nick':     "rebooped",
+        'chans':    ["#tests"],
         'auth':     (False, "password")
     },
     {
