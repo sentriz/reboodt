34749621cdc1f4ec19a794c72e12e9a847bd3818
Senan Kelly <senan@senan.xyz>
2015-02-21 16:31:25 +0000

    remove non-default servers from config

--- config.py
+++ config.py
@@ -1,24 +1,24 @@
-admins = ["sentriz", "sentriz_android"]
+admins = ["admin1", "admin2"]
 command_prefix = "."
 
 # add as many as you can handle
 servers = [
     {
-        'connect':  True,
-        'name':     "GeekShed",
-        'host':     "irc.geekshed.net",
+        'connect':  False,
+        'name':     "ExampleNet",
+        'host':     "irc.example.net",
         'port':     6667,
-        'nick':     "reboodt",
-        'chans':    ["#drpeen", "#testong"],
-        'auth':     (False, "password") #authenticate with NickServ? if True, type password too.
+        'nick':     "bot_nick",
+        'chans':    ["#channel1", "channel2"],
+        'auth':     (False, "password")
     },
     {
         'connect':  False,
-        'name':     "EsperNet",
-        'host':     "irc.esper.net",
+        'name':     "ExampleNet",
+        'host':     "irc.example.net",
         'port':     6667,
-        'nick':     "rebooped",
-        'chans':    ["#tests"],
+        'nick':     "bot_nick",
+        'chans':    ["#channel1", "channel2"],
         'auth':     (False, "password")
     },
     {
