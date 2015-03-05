b9af5553b6bd7cab16f7c02d0d8fdaa679d30a08
Senan Kelly <senan@senan.xyz>
2015-03-05 22:03:11 +0000

    fix google.py for when user gave no arguments, plugin would do nothing

--- files/plugins/google.py
+++ files/plugins/google.py
@@ -26,24 +26,30 @@ class Google(BasePlugin):
         
         return response_data
         
+    def _make_results_generator(self, response_data, user_query):       
+        results = response_data["results"]
+        result_count = response_data["cursor"]["resultCount"]
+        time = response_data["cursor"]["searchResultTime"]
+
+        yield '{0} results for "{1}" in {2}s:'.format(
+            result_count, user_query, time)
+        for result in results:
+            yield "{1} ({0})".format(
+                result["url"], result["titleNoFormatting"]
+            )    
  
-    def command_function(self, arguments, sender, channel):
+    def command_function(self, arguments, sender, channel): 
         if not arguments:
             return "please provide a search query"
             
         query = " ".join(arguments)
         response_data = self._google_search(query)
+        results_generator = self._make_results_generator(
+            response_data, query)
         
-        results = response_data["results"]
-        result_count = response_data["cursor"]["resultCount"]
-        time = response_data["cursor"]["searchResultTime"]
+        return results_generator
+        
 
-        yield '{0} results for "{1}" in {2}s'.format(
-            result_count, query, time)
-        for result in results:
-            yield "{1} ({0})".format(
-                result["url"], result["titleNoFormatting"]
-            )
         
 classes = (Google,)
         
