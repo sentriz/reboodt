27e57883dda2e2fd59939ebe21f6634ba2619552
Senan Kelly <senan@senan.xyz>
2015-03-11 22:31:05 +0000

    move methods from UserBot to Bot, remove unused imports, change printing of commands

--- reboodt/lib/bot.py
+++ reboodt/lib/bot.py
@@ -1,11 +1,12 @@
 # Based on Alec Hussey's PyBotlib
 
+import config
+import imp
+import logging
+import os
 import socket
 import sys
 import time
-import random
-import string
-import logging
 
 class Protocol:
     def __init__(self, server, port=6667):
@@ -81,12 +82,14 @@ class Protocol:
 
     def disconnect(self, message="disconnect"):
         self.send("QUIT :" + message)
-        #time.sleep(5.0)
-        #self.connection.shutdown(socket.SHUT_RDWR)
-        #self.connection.close()
+        time.sleep(5.0)
+        self.connection.shutdown(socket.SHUT_RDWR)
+        self.connection.close()
 
 class Bot():
-    def __init__(self, server, port, channels, nick, network_name, authentication):
+    def __init__(self, server, port, channels, 
+            nick, network_name, authentication):
+    
         # initialise IRC protocol
         self.protocol = Protocol(server, port)
         self.protocol.identify(nick)
@@ -96,17 +99,26 @@ class Bot():
         self.port = port
         self.channels = channels
         self.nick = nick
-        
-        self.in_channels = False
         self.network_name = network_name
         self.authentication = authentication
         
-        # initialise raw / command type / channel strings
+        # misc booleans
+        self.in_channels = False
+        
+        # plugin/help data/method calls 
+        self.commands = {}
+        self.variables = {}
+        self.help = {}
+        
+        self.load_plugins()
+        self.load_help()
+
+        # initialise raw/command-type/channel strings
         self.data = ""
         self.last_command_type = ""
         self.last_command_parsed = {}
         self.last_channel_message = ""
-
+        
     def _actions(self):
         """
         loop that listens and performs defined commands
@@ -120,25 +132,8 @@ class Bot():
             if not self.in_channels:
                 self._join_channels()
                 self.in_channels = True
-
-        elif self.last_command_type in ("message", "user_command"):
-            # force "message" in case last_command_type is "command"
-            parsed_command = self._parse_raw_command(parse_for="message")
-            
-            # ignore possibly junky startup messages
-            if not parsed_command["target"].startswith("#"):
-                return
                 
-            logging.info("[{0}] <{1}> {2}".format(
-                parsed_command["target"],
-                parsed_command["sender"],
-                parsed_command["message"]
-            ))
-            
-            # add the message to self.last_channel_message, 
-            # but not if it was a command
-            if not self.last_command_type == "user_command":
-                self.last_channel_message = parsed_command["message"]
+            return
 
         elif self.last_command_type == "notice":
             auth_or_not, password = self.authentication
@@ -148,13 +143,73 @@ class Bot():
 
             # hook NickServ asking for authentication
             # "this nickname is registered".. "please choose"..
-            if not "choose a different" in self.data:
+            if "choose a different" in self.data:
+                self.protocol.privmsg("NickServ", "identify " + password)
+                hidden_password = "*"*len(password)
+                
+                logging.info("identifed with NickServ with pass " + hidden_password)
+                
+            return
+        
+        elif self.last_command_type == "message":
+            
+            # ignore junky startup messages by ensuring
+            # - channel starts with "#"
+            if not self.last_command_parsed["target"].startswith("#"):
                 return
-            self.protocol.privmsg("NickServ", "identify " + password)
-            hidden_password = "*"*len(password)
             
-            logging.info("identifed with NickServ with pass " + hidden_password)
+            # print command or message
+            self._log_message(**self.last_command_parsed)
             
+            # add the last message to self.last_channel_message,
+            self.last_channel_message = self.last_command_parsed["message"]
+                
+        # run plugin if message was a command
+        elif self.last_command_type == "user_command":
+
+            if self.last_command_parsed["command"] in self.commands:
+                self._run_plugin(**self.last_command_parsed)
+            else:
+                # remap command to message and log because command was not valid
+                target = self.last_command_parsed["channel"]
+                sender = self.last_command_parsed["sender"]
+                message = self.last_command_parsed["command"] + \
+                    " " + " ".join(self.last_command_parsed["arguments"])
+                message = message.rstrip()
+                self._log_message(target, sender, message)
+    
+    def _run_plugin(self, command, sender, arguments, channel):
+            
+        logging.info('>> {0} issued command "{1} {2}" from {3}'.format(
+            sender, command, " ".join(arguments), channel))
+            
+        plugin = self.commands[command]
+        if plugin.needs_admin and sender not in config.admins:
+            sorry_string = 'sorry, you need to be an admin to use the "{0}" plugin'
+            self.protocol.privmsg(sender, sorry_string.format(plugin.name))
+            return
+
+        evaluated_arguments = self._evaluate_arguments(arguments)
+        try:
+            command_output = plugin.command_function(
+                evaluated_arguments, sender, channel)
+        except Exception as exc:
+            self.say("error: {0}".format(str(exc).lower()))
+            logging.exception(exc)
+            return
+            
+        command_output_type = type(command_output).__name__
+        if command_output_type == "str":
+            self.say(command_output)
+            
+        elif command_output_type in ("generator", "list"):
+            for element in command_output:
+                self.say(element)
+                
+        else:
+            self.error('plugin "{0}" returns an unknown object type "{1}"'.format(
+                plugin.name, command_output_type))
+        
     def _join_channels(self):
 
         for channel in self.channels:
@@ -220,19 +275,109 @@ class Bot():
             else:
                 if result == raw_type:
                     return type_name
+                    
+    def _evaluate_arguments(self, arguments):
+        """
+        evaluate a list of command arguments for a command
+        eg. "the current time is .time" becomes
+            "the current time is [current time]"
+        """
+
+        arguments_to_return = []
+
+        for argument in arguments:
+            if argument not in self.variables:
+                arguments_to_return += [argument]
+            else:
+                variable = argument
+                plugin = self.variables[variable]
+                variable_value = plugin.variable_function()
+                arguments_to_return += [variable_value]
+
+        return arguments_to_return
+
+    def _get_help(self, command):
+    
+        command_for_help = "." + command
+        for c_or_v, list in self.help.items():
+            for help_string in list:
+                if command_for_help in help_string:
+                    self.say("help for {0} {1}:".format(
+                        c_or_v, command_for_help))
+                    self.say(help_string)
+                    return
+                    
+        self.say('error: could not find help for "{0}"'.format(
+            command_for_help))
+            
+    def _log_message(self, target, sender, message):
+        logging.info("[{target}] <{sender}> {message}".format(
+            **vars()))
+
+    def load_help(self):
+    
+        help_file_name = "help.txt"
+        
+        current_folder = os.path.dirname(__file__)
+        provisional_path = os.path.join(current_folder, os.pardir)
+        parent_folder = os.path.abspath(provisional_path)
+        help_file = os.path.join(parent_folder, "files", help_file_name)
+        
+        self.help = {}
+
+        with open(help_file) as file:
+            for line in file:
+                line = line.rstrip()
+                if line.startswith("#"):
+                    c_or_v = line.strip("#")
+                    continue
+                if c_or_v not in self.help:
+                    self.help[c_or_v] = list = []
+                list.append(line)
+                
+        logging.info('loaded help from file "{0}"'.format(help_file_name))
+
+    def load_plugins(self):
+        """
+        load all plugins in the "plugins" directory and add
+        the command and variable functions in them to self.commands
+        and self.variables
+        """
+    
+        current_folder = os.path.dirname(__file__)
+        provisional_path = os.path.join(current_folder, os.pardir)
+        parent_folder = os.path.abspath(provisional_path)
+        plugin_folder = os.path.join(parent_folder, "plugins")
+
+        for file in os.listdir(plugin_folder):
+
+            if file.startswith("_"):
+                continue
+            elif not file.endswith(".py"):
+                continue
+
+            plugin_path = os.path.join(plugin_folder, file)
+            plugin_file = imp.load_source(file, plugin_path)
+
+            for class_ in plugin_file.classes:
+                plugin = class_(self)
+                if hasattr(plugin, "variable"):
+                    self.variables[plugin.variable] = plugin
+                elif hasattr(plugin, "command"):
+                    self.commands[plugin.command] = plugin
+                logging.info('loaded plugin "{0}" from file "{1}"'.format(
+                    plugin.name, file))
 
     def say(self, message, channel=None):
     
         if not channel:
             parsed_string = self._parse_raw_command(parse_for="message")
             channel = parsed_string["target"]
-
+        
+        # send message
         self.protocol.privmsg(channel, message)
-        logging.info("[{0}] <{1}> {2}".format(
-            channel,
-            self.nick,
-            message
-        ))
+        logging.info('<< replied "{0}"'.format(message))
+        # update self.last_channel_message
         self.last_channel_message = message
 
     def run(self):
