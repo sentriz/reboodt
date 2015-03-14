110d7bef77ba094ad0c7abc2aa6e713089173bdd
sentriz <senan.f.b.kelly+github@gmail.com>
2015-03-14 19:09:06 +0000

    add PluginManager to lib/parsing and integrate

--- reboodt/lib/bot.py
+++ reboodt/lib/bot.py
@@ -1,9 +1,9 @@
-# based on Alec Hussey's PyBotlib
+# Bot() based on Alec Hussey's PyBotlib
 
 import config
-import imp
 from lib.irc import Protocol
 from lib.parsing import IRCString
+from lib.parsing import PluginManager
 import logging
 import os
 import sys
@@ -18,6 +18,11 @@ class Bot():
         # initialise IRC protocol
         self.protocol = Protocol(server, port)
         self.protocol.identify(nick)
+        
+        # initialise plugins
+        self.plugins = PluginManager()
+        self.plugins.load()
+        self.plugins.load_help()
 
         # initialise class variables
         self.server = server
@@ -30,19 +35,6 @@ class Bot():
         # misc booleans
         self.in_channels = False
 
-        # plugin/help data/method calls
-        self.commands = {}
-        self.variables = {}
-        self.help = {}
-
-        self.load_plugins()
-        self.load_help()
-
-        # initialise raw/command-type/channel strings
-        self.raw_string = ""
-        self.last_string_type = ""
-        self.last_string_parsed = {}
-        self.last_channel_message = ""
 
     def _actions(self):
         """
@@ -87,48 +79,30 @@ class Bot():
         # run plugin if message was a command
         elif self.last_string_type == "user_command":
 
-            if self.last_string_parsed["command"] in self.commands:
-                self._run_plugin(**self.last_string_parsed)
+            logging.info('>> {0} issued command "{1}" from {2}'.format(
+                self.last_string_parsed["sender"], 
+                self.last_message_parsed["message"], 
+                self.last_message_parsed["target"]
+                )
+            )
+            if self.last_string_parsed["command"] in self.plugins.commands:
+                command_output = self.plugins.run(**self.last_string_parsed)
+                command_output_type = type(command_output).__name__
+                
+                if command_output_type == "str":
+                    self.say(command_output)
+                elif command_output_type in ("generator", "list"):
+                    for line in command_output:
+                        self.say(line)
+                else:
+                    logging.error('plugin "{0}" returns an unknown object type "{1}"'.format(
+                        plugin.name, command_output_type))
+                
             else:
-                # remap command to message and log because command was not valid
-                target = self.last_string_parsed["channel"]
-                sender = self.last_string_parsed["sender"]
-                message = self.last_string_parsed["command"] + \
-                    " " + " ".join(self.last_string_parsed["arguments"])
-                message = message.rstrip()
-                self._log_message(target, sender, message)
-
-    def _run_plugin(self, command, sender, arguments, channel):
-
-        logging.info('>> {0} issued command "{1} {2}" from {3}'.format(
-            sender, command, " ".join(arguments), channel))
-
-        plugin = self.commands[command]
-        if plugin.needs_admin and sender not in config.admins:
-            sorry_string = 'sorry, you need to be an admin to use the "{0}" plugin'
-            self.protocol.privmsg(sender, sorry_string.format(plugin.name))
-            return
-
-        evaluated_arguments = self._evaluate_arguments(arguments)
-        try:
-            command_output = plugin.command_function(
-                evaluated_arguments, sender, channel)
-        except Exception as exc:
-            self.say("error: {0}".format(str(exc).lower()))
-            logging.exception(exc)
-            return
-
-        command_output_type = type(command_output).__name__
-        if command_output_type == "str":
-            self.say(command_output)
-
-        elif command_output_type in ("generator", "list"):
-            for element in command_output:
-                self.say(element)
-
-        else:
-            self.error('plugin "{0}" returns an unknown object type "{1}"'.format(
-                plugin.name, command_output_type))
+                logging.info('<< "{0}" is not a plugin command'.format(
+                    self.last_string_parsed["command"]
+                    )
+                )
 
     def _join_channels(self):
 
@@ -138,30 +112,11 @@ class Bot():
             self.protocol.join(channel)
             logging.info('joined channel "{0}"'.format(channel))
 
-    def _evaluate_arguments(self, arguments):
-        """
-        evaluate a list of command arguments for a command
-        eg. "the current time is .time" becomes
-            "the current time is [current time]"
-        """
-
-        arguments_to_return = []
-
-        for argument in arguments:
-            if argument not in self.variables:
-                arguments_to_return += [argument]
-            else:
-                variable = argument
-                plugin = self.variables[variable]
-                variable_value = plugin.variable_function()
-                arguments_to_return += [variable_value]
-
-        return arguments_to_return
-
     def _get_help(self, command):
 
         command_for_help = "." + command
-        for c_or_v, list in self.help.items():
+        
+        for c_or_v, list in self.plugins.help.items():
             for help_string in list:
                 if command_for_help in help_string:
                     self.say("help for {0} {1}:".format(
@@ -175,60 +130,6 @@ class Bot():
     def _log_message(self, target, sender, message):
         logging.info("[{target}] <{sender}> {message}".format(
             **vars()))
-
-    def load_help(self):
-
-        help_file_name = "help.txt"
-
-        current_folder = os.path.dirname(__file__)
-        provisional_path = os.path.join(current_folder, os.pardir)
-        parent_folder = os.path.abspath(provisional_path)
-        help_file = os.path.join(parent_folder, "files", help_file_name)
-
-        self.help = {}
-
-        with open(help_file) as file:
-            for line in file:
-                line = line.rstrip()
-                if line.startswith("#"):
-                    c_or_v = line.strip("#")
-                    continue
-                if c_or_v not in self.help:
-                    self.help[c_or_v] = list = []
-                list.append(line)
-
-        logging.info('loaded help from file "{0}"'.format(help_file_name))
-
-    def load_plugins(self):
-        """
-        load all plugins in the "plugins" directory and add
-        the command and variable functions in them to self.commands
-        and self.variables
-        """
-
-        current_folder = os.path.dirname(__file__)
-        provisional_path = os.path.join(current_folder, os.pardir)
-        parent_folder = os.path.abspath(provisional_path)
-        plugin_folder = os.path.join(parent_folder, "plugins")
-
-        for file in os.listdir(plugin_folder):
-
-            if file.startswith("_"):
-                continue
-            elif not file.endswith(".py"):
-                continue
-
-            plugin_path = os.path.join(plugin_folder, file)
-            plugin_file = imp.load_source(file, plugin_path)
-
-            for class_ in plugin_file.classes:
-                plugin = class_(self)
-                if hasattr(plugin, "variable"):
-                    self.variables[plugin.variable] = plugin
-                elif hasattr(plugin, "command"):
-                    self.commands[plugin.command] = plugin
-                logging.info('loaded plugin "{0}" from file "{1}"'.format(
-                    plugin.name, file))
 
     def say(self, message, channel=None):
 
@@ -256,12 +157,12 @@ class Bot():
                 sys.exit(1)
 
             # PRIVMSG, NOTICE, ect.
-            irc_string = IRCString(self.raw_string)
-            self.last_string_type = irc_string._get_string_type()
-            self.last_string_parsed = irc_string._parse_string()
+            self.string = IRCString(self.raw_string)
+            self.last_string_type = self.string._get_type()
+            self.last_string_parsed = self.string._parse()
 
             if self.last_string_type == "user_command":
-                self.last_message_parsed = irc_string._parse_string(
+                self.last_message_parsed = self.string._parse(
                     parse_for="message")
 
             # perform basic actions like pong, ect.
--- reboodt/lib/parsing.py
+++ reboodt/lib/parsing.py
@@ -1,12 +1,17 @@
+import logging
+import os
+import imp
+import config
+
 class IRCString:
 
     def __init__(self, raw_string):
-    
+
         self.raw_string = raw_string
 
-    def _parse_string(self, parse_for=None):
+    def _parse(self, parse_for=None):
 
-        parse_for = parse_for or self._get_string_type()
+        parse_for = parse_for or self._get_type()
 
         to_parse = {
             'message': {
@@ -37,7 +42,7 @@ class IRCString:
                 to_return[part] = parse(self.raw_string)
             return to_return
 
-    def _get_string_type(self):
+    def _get_type(self):
 
         # not using dictionary because order is important here
         # "user_command" must always come before "message"
@@ -58,3 +63,101 @@ class IRCString:
             else:
                 if result == raw_type:
                     return type_name
+
+class PluginManager:
+
+    def __init__(self):
+        self.commands = {}
+        self.variables = {}
+        self.help = {}
+
+    def load_help(self):
+
+        help_file_name = "help.txt"
+
+        current_folder = os.path.dirname(__file__)
+        provisional_path = os.path.join(current_folder, os.pardir)
+        parent_folder = os.path.abspath(provisional_path)
+        help_file = os.path.join(parent_folder, "files", help_file_name)
+
+        self.help = {}
+
+        with open(help_file) as file:
+            for line in file:
+                line = line.rstrip()
+                if line.startswith("#"):
+                    c_or_v = line.strip("#")
+                    continue
+                if c_or_v not in self.help:
+                    self.help[c_or_v] = list = []
+                list.append(line)
+
+        logging.info('loaded help from file "{0}"'.format(help_file_name))
+
+    def load(self):
+        """
+        load all plugins in the "plugins" directory and add
+        the command and variable functions in them to self.commands
+        and self.variables
+        """
+
+        current_folder = os.path.dirname(__file__)
+        provisional_path = os.path.join(current_folder, os.pardir)
+        parent_folder = os.path.abspath(provisional_path)
+        plugin_folder = os.path.join(parent_folder, "plugins")
+
+        for file in os.listdir(plugin_folder):
+
+            if file.startswith("_"):
+                continue
+            elif not file.endswith(".py"):
+                continue
+
+            plugin_path = os.path.join(plugin_folder, file)
+            plugin_file = imp.load_source(file, plugin_path)
+
+            for class_ in plugin_file.classes:
+                plugin = class_(self)
+                if hasattr(plugin, "variable"):
+                    self.variables[plugin.variable] = plugin
+                elif hasattr(plugin, "command"):
+                    self.commands[plugin.command] = plugin
+                logging.info('loaded plugin "{0}" from file "{1}"'.format(
+                    plugin.name, file))
+
+    def _evaluate_arguments(self, arguments):
+        """
+        evaluate a list of command arguments for a command
+        eg. "the current time is .time" becomes
+            "the current time is [current time]"
+        """
+
+        arguments_to_return = []
+
+        for argument in arguments:
+            if argument not in self.variables:
+                arguments_to_return += [argument]
+            else:
+                variable = argument
+                plugin = self.variables[variable]
+                variable_value = plugin.variable_function()
+                arguments_to_return += [variable_value]
+
+        return arguments_to_return
+
+    def run(self, command, sender, arguments, channel):
+
+        plugin = self.commands[command]
+        if plugin.needs_admin and sender not in config.admins:
+            sorry_string = '{0}: you need to be an admin to use the "{0}" plugin'
+            return sorry_string.format(sender, plugin.name)
+
+        evaluated_arguments = self._evaluate_arguments(arguments)
+        try:
+            command_output = plugin.command_function(
+                evaluated_arguments, sender, channel)
+        except Exception as exc:
+            logging.exception(exc)
+            return "error: {0}".format(str(exc).lower())
+            
+        return command_output
