510deba52fb0bd3df6b8a6759a1e872c586d7cd4
sentriz <senan.f.b.kelly+github@gmail.com>
2015-03-15 19:43:27 +0000

    add utilities.py and integrate

---
+++ reboodt/lib/utilities.py
@@ -0,0 +1,16 @@
+import yaml
+import os
+
+def load_yaml(path_relative_to_reboodt):
+
+    current_folder = os.path.dirname(__file__)
+    provisional_path = os.path.join(current_folder, os.pardir)
+    parent_folder = os.path.abspath(provisional_path)
+    yaml_file = os.path.join(
+        parent_folder, path_relative_to_reboodt)
+
+    with open(yaml_file, "r") as file:
+        yaml_contents = yaml.load(file)
+
+    return yaml_contents
+
