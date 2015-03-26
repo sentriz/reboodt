d372113b5f60847ccaeb8d9f08c03b044be6dddf
sentriz <senan.f.b.kelly+github@gmail.com>
2015-03-26 23:47:14 +0000

    trim trailing

--- reboodt/plugins/python.py
+++ reboodt/plugins/python.py
@@ -3,13 +3,13 @@ import urllib.parse
 
 class Python(BaseCommand):
     """
-    reboodt plugin that evaluates a python expression 
+    reboodt plugin that evaluates a python expression
     usage: .py 1 + 2
     result: 3
     """
-        
+
     command = ".py"
-        
+
     def _evaluate(self, string):
         base_url = "http://tumbolia.appspot.com/py/"
         url = base_url + urllib.parse.quote(string)
@@ -18,17 +18,16 @@ class Python(BaseCommand):
         response = encoded_response.decode()
         response = response.rstrip()
         return response
-        
+
     def command_function(self, arguments, sender, channel):
         string = " ".join(arguments)
         if not string:
             return "please provide a valid python expression"
         result = self._evaluate(string)
         return result
-        
+
 classes = (Python,)
-        
+
 if __name__ == "__main__":
     for class_ in classes:
         print(class_.__doc__)
-        
