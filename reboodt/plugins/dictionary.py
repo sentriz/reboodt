8dfaf28b5fce31944bfc34d2cdacc9f3b5b3e118
sentriz <senan.f.b.kelly+github@gmail.com>
2015-03-31 01:10:52 +0100

    strip definitions of '; '

--- reboodt/plugins/dictionary.py
+++ reboodt/plugins/dictionary.py
@@ -14,18 +14,19 @@ class MerriamWebster(BaseCommand):
         api_key = self.api_keys["merriam_webster_collegiate"]
         dictionary = CollegiateDictionary(api_key)
         definitions = []
+        
         try:
             for entry in dictionary.lookup(query):
                 for definition, examples in entry.senses:
-                    if len(definitions) > 4:
+                    if len(definitions) >= 3:
                         break
                     to_append = (entry.word, entry.function, definition)
                     definitions.append(to_append)
         except WordNotFoundException as exc:
-            return_string = 'no definitions found for "{0}".'.format(query)
+            return_string = 'no definitions found for "{0}"'.format(query)
             if exc.suggestions:
                 short_suggestions = exc.suggestions[:5]
-                return_string += " did you mean "
+                return_string += ". did you mean "
                 if len(short_suggestions) == 1:
                     return_string += '"' + short_suggestions[0] + '"'
                 else:
@@ -34,8 +35,10 @@ class MerriamWebster(BaseCommand):
                 return_string += "?"
             yield return_string
             return
+            
         for word, function, definition in definitions:
-            yield "[{0}|{1}] {2}".format(word, function, definition)
+            definition_string = definition.strip("; ")
+            yield "[{0}|{1}] {2}".format(word, function, definition_string)
 
     def command_function(self, arguments, sender, channel):
         query = " ".join(arguments)
@@ -43,10 +46,11 @@ class MerriamWebster(BaseCommand):
             return "please provide a word to define"
         if not "merriam_webster_collegiate" in self.api_keys:
             api_register_url = "http://www.dictionaryapi.com/register/index.htm"
-            string = "please get a Merriam Webster Collegiate API key"
-            string += " " + self._shorten_url(api_register_url)
+            string = "please get a Merriam Webster Collegiate API key: "
+            string += self._shorten_url(api_register_url)
             return string
         definitions = self._lookup(query)
+        
         return definitions
 
 classes = (MerriamWebster,)
